ec11aff859e09850e220afbb9d85d52b188f47c2
PawanKalyanDieHardFan13 <abhiramvulimiri23@gmail.com>
2026-09-06 19:07:39 +0530

    Add databaseURL to Firebase configuration

--- firebase-config.js
+++ firebase-config.js
@@ -1,7 +1,8 @@
 window.SKILLSWAP_FIREBASE_CONFIG = {
     apiKey: "AIzaSyCI3r2qw_xHMwI4hH04PeXTxoq7euQR508",
     authDomain: "skillswap-india-ae59f.firebaseapp.com",
-  projectId: "skillswap-india-ae59f",
+    databaseURL: "https://skillswap-india-ae59f-default-rtdb.firebaseio.com",
+    projectId: "skillswap-india-ae59f",
     storageBucket: "skillswap-india-ae59f.firebasestorage.app",
     messagingSenderId: "479580370395",
     appId: "1:479580370395:web:6024bc5162a7d05cd963fe"
