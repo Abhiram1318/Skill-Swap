9d9dc7c9b4d4ec10bc40200298428b559e92c07c
PawanKalyanDieHardFan13 <abhiramvulimiri23@gmail.com>
2026-09-09 21:30:55 +0530

    Delete firebase-config.js

--- firebase-config.js
+++
@@ -1,9 +0,0 @@
-window.SKILLSWAP_FIREBASE_CONFIG = {
-    apiKey: "AIzaSyCI3r2qw_xHMwI4hH04PeXTxoq7euQR508",
-    authDomain: "skillswap-india-ae59f.firebaseapp.com",
-    databaseURL: "https://skillswap-india-ae59f-default-rtdb.asia-southeast1.firebasedatabase.app",
-    projectId: "skillswap-india-ae59f",
-    storageBucket: "skillswap-india-ae59f.firebasestorage.app",
-    messagingSenderId: "479580370395",
-    appId: "1:479580370395:web:6024bc5162a7d05cd963fe"
-};
